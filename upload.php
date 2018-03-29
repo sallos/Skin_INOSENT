e19b7cf06ee84e58f0424320a14d4a89801b5006
sallos <lars@mordenham.de>
2018-03-29 18:24:19 +0200

    111

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "./img/";
-$target_file = $target_dir . "test.png");
+$target_file = $target_dir . "test.png";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 echo $target_file;
@@ -20,7 +20,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". $target_file " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
